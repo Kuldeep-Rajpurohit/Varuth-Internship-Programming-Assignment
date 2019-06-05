85800c2ecdc52e69e460f19c886bb0f0da918179
Kuldeep-SinghR <51077392+Kuldeep-SinghR@users.noreply.github.com>
2019-06-05 23:58:34 +0530

    Update Third(3co-ord).c

--- Third(3co-ord).c
+++ Third(3co-ord).c
@@ -1,3 +1,13 @@
+/**
+ Consider three dimension system. A point in three dimension system is represented as ( x , y , z ) ,
+where x , y and z are the x , y and z coordinates of the point in three dimension space respectively.
+Write a program that:
+a) Reads from end user using suitable messages :
+i. Point p1 ( x1 , y1 , z1 ) , and
+ii. Point p2 ( x2 , y2 , z2 )
+b) Prints with suitable message all partial derivatives between the points ( p1 , p2 )
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,3 +24,12 @@ int main()
     printf("The function f(ln)= (%.2fx) +(%.2fy)+(%.2fz)",x,y,z);
     return 0;
 }
+
+/**
+OUTPUT:
+Enter the x,y and z coordinates of the point 1 : 1 5 3
+
+Enter the x,y and z coordinates of the point 2 : 3 4 1
+The function f(ln)= (2.00x) +(-1.00y)+(-2.00z)
+
+**/
